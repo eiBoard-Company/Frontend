62464614d523d60c1b154642664b3cbf5e3cd54d
Matteo Staar <matteo.staar@cjt.de>
2022-11-03 12:45:05 +0100

    adding backend access

--- eiboard_flutter/lib/pages/components/backend_rapla.dart
+++ eiboard_flutter/lib/pages/components/backend_rapla.dart
@@ -1,10 +1,14 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' show utf8;
 
-
-/*Future<http.Response> fetchData() async {
-  final response = await http.get(
-      Uri.parse('http://localhost:8090/lectures/2022-11-02'),
-      headers: {"Access-Control_Allow_Origin": "*"});
-  print(response.statusCode);
-  print("test " + response.body);
-  return response;
-} */
+class HttpRequest {
+  Future<http.Response> fetchData() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:8090/lectures/2022-11-02'));
+    var encoded = utf8.encode(response.body);
+    var decoded = utf8.decode(encoded);
+    print(response.statusCode);
+    print("test " + decoded);
+    return response;
+  }
+}
