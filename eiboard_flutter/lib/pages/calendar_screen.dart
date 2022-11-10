944ec9e0c5d144fa2dea184b77125c8106d05ce7
efahrner <eileen.fahrner@swr.de>
2022-11-10 11:34:07 +0100

    added page background class

--- eiboard_flutter/lib/pages/calendar_screen.dart
+++ eiboard_flutter/lib/pages/calendar_screen.dart
@@ -1,66 +1,24 @@
+import 'package:eiboard_flutter/pages/components/page.dart';
 import 'package:flutter/material.dart';
 import '../themes/light_standard_theme.dart';
 
-class CalendarScreen extends StatefulWidget {
+class CalendarScreen extends StatelessWidget {
   const CalendarScreen({Key? key}) : super(key: key);
 
   @override
-  State<CalendarScreen> createState() => _CalendarScreenState();
-}
-
-class _CalendarScreenState extends State<CalendarScreen> {
-  @override
   Widget build(BuildContext context) {
-    // Size size = MediaQuery.of(context).size; //getting size of screen  need to implement
-    return Scaffold(
-        extendBodyBehindAppBar: true,
-        backgroundColor: LightStandardTheme.colorMain,
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(70),
-          child: SafeArea(
-            child: AppBar(
-              elevation: 0,
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back, color: Colors.black),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-              centerTitle: true,
-              title: const Text(
-                "Schedule",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    color: Colors.black),
-              ),
-              backgroundColor: LightStandardTheme.colorMain,
+    return PageBackground(
+        topic: 'Schedule',
+        child: Center(
+            child: Column(
+          children: const [
+            SizedBox(height: 20),
+            Text(
+              'October',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-          ),
-        ),
-        body: SafeArea(
-            child: Center(
-                child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Expanded(
-                child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius:
-                          BorderRadius.vertical(top: Radius.circular(40)),
-                      color: Colors.white,
-                    ),
-                    child: Center(
-                        child: Column(
-                      children: const [
-                        SizedBox(height: 20),
-                        Text(
-                          'October',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
-                        ),
-                        SizedBox(height: 20),
-                      ],
-                    ))))
+            SizedBox(height: 20),
           ],
-        ))));
+        )));
   }
 }
