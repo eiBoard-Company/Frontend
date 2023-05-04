f3ce41810a33468382776f115e4806c3b6d6f9be
eileen522 <eileen.fahrner@swr.de>
2023-05-04 12:40:19 +0200

    adjusted todo_list_box for main screen

--- eiboard_flutter/lib/pages/components/todo_list_box.dart
+++ eiboard_flutter/lib/pages/components/todo_list_box.dart
@@ -7,8 +7,33 @@ class TodoListBox extends StatelessWidget {
   final String classname;
   final String date;
   final Color color;
+  final double width;
+  final double height;
+  final double fontSizeTitleOfTodo;
+  final double fontSizeCategoryOfTodo;
+  final bool showDueDateOfTodo;
+
   const TodoListBox(this.taskname, this.classname, this.date, this.color,
-      {super.key});
+      {this.width = 337,
+      this.height = 58,
+      this.fontSizeTitleOfTodo = 14,
+      this.fontSizeCategoryOfTodo = 10,
+      this.showDueDateOfTodo = true,
+      Key? key})
+      : super(key: key);
+
+  const TodoListBox.withSize(
+      this.taskname,
+      this.classname,
+      this.date,
+      this.color,
+      this.width,
+      this.height,
+      this.fontSizeTitleOfTodo,
+      this.fontSizeCategoryOfTodo,
+      this.showDueDateOfTodo,
+      {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +41,8 @@ class TodoListBox extends StatelessWidget {
       borderRadius: BorderRadius.circular(15),
       child: Container(
         color: color,
-        width: 337,
-        height: 58,
+        width: width,
+        height: height,
         padding: const EdgeInsets.all(15.0),
         child: Center(
             child: Stack(
@@ -26,8 +51,8 @@ class TodoListBox extends StatelessWidget {
                 alignment: Alignment.topLeft,
                 child: Text(taskname,
                     style: GoogleFonts.montserrat(
-                      textStyle: const TextStyle(
-                          fontSize: 14,
+                      textStyle: TextStyle(
+                          fontSize: fontSizeTitleOfTodo,
                           fontWeight: FontWeight.w700,
                           color: LightStandardTheme.colorLightFont),
                     ))),
@@ -35,20 +60,21 @@ class TodoListBox extends StatelessWidget {
                 alignment: Alignment.bottomLeft,
                 child: Text(classname,
                     style: GoogleFonts.montserrat(
-                      textStyle: const TextStyle(
-                          fontSize: 10,
+                      textStyle: TextStyle(
+                          fontSize: fontSizeCategoryOfTodo,
                           fontWeight: FontWeight.w700,
                           color: LightStandardTheme.colorSubjectName),
                     ))),
-            Align(
-                alignment: Alignment.topRight,
-                child: Text(date,
-                    style: GoogleFonts.montserrat(
-                      textStyle: const TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.w700,
-                          color: LightStandardTheme.colorSubjectName),
-                    )))
+            if (showDueDateOfTodo)
+              Align(
+                  alignment: Alignment.topRight,
+                  child: Text(date,
+                      style: GoogleFonts.montserrat(
+                        textStyle: TextStyle(
+                            fontSize: fontSizeCategoryOfTodo,
+                            fontWeight: FontWeight.w700,
+                            color: LightStandardTheme.colorSubjectName),
+                      )))
           ],
         )),
       ),
