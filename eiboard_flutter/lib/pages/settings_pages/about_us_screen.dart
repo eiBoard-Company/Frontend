00d48b841e53813bdf6af0462386c212c1a62e58
eileen522 <eileen.fahrner@swr.de>
2023-05-05 22:47:06 +0200

    added settings page + linked pages

---
+++ eiboard_flutter/lib/pages/settings_pages/about_us_screen.dart
@@ -0,0 +1,25 @@
+import 'package:eiboard_flutter/pages/components/page.dart';
+import 'package:flutter/material.dart';
+
+class AboutUsScreen extends StatelessWidget {
+  const AboutUsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageBackground(
+        topic: 'About us',
+        showPlusIcon: false,
+        child: Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 30),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    SizedBox(
+                      height: 30,
+                    ),
+                  ]),
+            )));
+  }
+}
