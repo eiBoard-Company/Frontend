4c767ee8fdcf18e7cb2e2c9df3b50b01865dc7c4
Niklas Geppert <Niklas.Geppert@cas.de>
2023-05-23 13:49:39 +0200

    added Edit button, change DatePicker field

--- eiboard_flutter/lib/pages/singletask_screen.dart
+++ eiboard_flutter/lib/pages/singletask_screen.dart
@@ -1,9 +1,10 @@
-import 'package:date_field/date_field.dart';
 import 'package:eiboard_flutter/pages/components/page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../themes/light_standard_theme.dart';
+import 'components/button.dart';
+import 'components/custom_date_picker.dart';
 import 'components/task_List_Object.dart';
 
 class SingleTaskScreen extends StatefulWidget {
@@ -50,6 +51,7 @@ class _SingleTaskScreen extends State<SingleTaskScreen> {
         dueValue: "Overdue",
         typId: "4"),
   ];
+  final _dateController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -108,23 +110,11 @@ class _SingleTaskScreen extends State<SingleTaskScreen> {
                 Row(
                   children: [
                     Expanded(
-                      child: DateTimeFormField(
-                        decoration: const InputDecoration(
-                          hintStyle: TextStyle(
-                              color: LightStandardTheme.colorLightFont,
-                              fontSize: 12),
-                          errorStyle: TextStyle(
-                              color: LightStandardTheme.colorError,
-                              fontSize: 12),
-                        ),
-                        mode: DateTimeFieldPickerMode.time,
-                        autovalidateMode: AutovalidateMode.always,
-                        enabled: bearbeitet,
-                        initialValue: task.time,
-                        validator: (e) => (e?.day ?? 0) == 1
-                            ? 'Please not the first day'
-                            : null,
-                        onDateSelected: (DateTime value) {},
+                      child: CustomDatePicker(
+                        controller: _dateController,
+                        labelText: 'Date',
+                        disabled: !bearbeitet,
+                        initialDate: task.time,
                       ),
                     ),
                     const SizedBox(width: 10),
@@ -149,19 +139,50 @@ class _SingleTaskScreen extends State<SingleTaskScreen> {
                 ),
                 const SizedBox(height: 20),
                 Text('${sliderValue.toStringAsFixed(0)}% complete'),
-                Slider(
-                  value: sliderValue,
-                  min: 0.0,
-                  max: 100.0,
-                  divisions: 5,
-                  label: sliderValue.round().toString(),
-                  onChanged: (double value) {
-                    setState(() {
-                      sliderValue = value;
-                    });
-                  },
-                  activeColor: LightStandardTheme.colorSecondary,
-                  inactiveColor: LightStandardTheme.colorPrimary,
+                SliderTheme(
+                  data: const SliderThemeData(
+                      disabledThumbColor: LightStandardTheme.tagBorder,
+                      disabledActiveTrackColor:
+                          LightStandardTheme.colorSecondary,
+                      disabledInactiveTrackColor:
+                          LightStandardTheme.colorPrimary),
+                  child: Slider(
+                    value: sliderValue,
+                    min: 0.0,
+                    max: 100.0,
+                    divisions: 5,
+                    label: sliderValue.round().toString(),
+                    onChanged: bearbeitet
+                        ? (double value) {
+                            setState(() {
+                              task.completeValue = value;
+                            });
+                          }
+                        : null,
+                    onChangeEnd: (double value) {
+                      setState(() {
+                        task.completeValue = value;
+                      });
+                    },
+                    activeColor: LightStandardTheme.colorSecondary,
+                    inactiveColor: LightStandardTheme.colorPrimary,
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Button(
+                        bearbeitet ? 'Done' : 'Edit',
+                        () {
+                          setState(() {
+                            bearbeitet = !bearbeitet;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
                 ),
               ]),
         )));
